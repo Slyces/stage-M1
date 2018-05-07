5222be5eb3c77bddef9d67b8de46001283a2047b
Slyces <slyces.coding@gmail.com>
2018-05-07 15:27:41 +0200

    première version de simulation de routeurs opérationelle

--- simulation/src/main.py
+++ simulation/src/main.py
@@ -2,7 +2,14 @@
 __author__ = "Simon Lassourreuille"
 
 # --------------------------------- imports ---------------------------------- #
-import networkx as nx
+import networkx as nx, threading, queue, random
+from time import sleep
+
+# --------------------------------- globals ---------------------------------- #
+STACK_SIZE = None
+QUEUE_SIZE = 100
+
+# threading.stack_size(STACK_SIZE)
 
 # --------------------------- utilitary functions ---------------------------- #
 def make_symmetric(graph):
@@ -15,13 +22,79 @@ def make_symmetric(graph):
     return new_graph
 
 # --------------------------------- classes ---------------------------------- #
+class Node(threading.Thread):
+
+    def __init__(self, node, simulation, *args, **kwargs):
+        threading.Thread.__init__(self, *args, **kwargs)
+        self.node = node
+        self.simulation = simulation
+
+    def on_receive(self, sender, item):
+        print("{} received load {} from {}".format(
+                self.getName(),
+                item,
+                sender
+            ))
+        neighbors = [x for x in self.simulation.graph[self.node].keys()]
+        next_hop = random.choice(neighbors)
+        sleep(1)
+        self.send(next_hop, item)
+
+    def notify(self, receiver):
+        self.simulation.conditions[receiver].acquire()
+        self.simulation.conditions[receiver].notify()
+        self.simulation.conditions[receiver].release()
+
+    def send(self, receiver, item):
+        self.simulation.queues[self.node, receiver].put(item)
+        self.notify(receiver)
+
+    def run(self):
+        node = self.node
+        self.simulation.conditions[node].acquire()
+        while True:
+            self.simulation.conditions[node].wait()
+            for sender in self.simulation.graph[node]:
+                if not self.simulation.queues[sender, node].empty():
+                    self.on_receive(sender, self.simulation.queues[sender, node].get_nowait())
+
 class Simulation(object):
 
     def __init__(self, di_graph):
         """
         Generates a data structure to implement routing on the given graph
+        Creates a thread for each node of the graph. Two threads are able to
+        communicate if and only if their relatives nodes are connected.
 
         Args:
             di_graph: A symmetrical, directed graph from nx_graph (nx.DiGraph)
         """
         assert isinstance(di_graph, nx.DiGraph)
+        self.graph = di_graph
+
+        self.threads = {}
+        self.conditions = {}
+        for node in self.graph.nodes_iter():
+            new_thread = Node(node, self)
+
+            new_thread.setName("Thread {}".format(node))
+            self.threads[node] = new_thread
+            self.conditions[node] = threading.Condition()
+
+        self.queues = {}
+        for a, b in self.graph.edges_iter():
+            self.queues[a, b] = queue.Queue(QUEUE_SIZE)
+
+
+    def start(self):
+        for thread_num in self.threads:
+            self.threads[thread_num].start()
+
+        x = self.graph.nodes()[0]
+        y = self.graph[x].keys()[0]
+
+        self.queues[x, y].put("<message>")
+        self.conditions[y].acquire()
+        self.conditions[y].notify()
+        self.conditions[y].release()
+
--- simulation/src/tests.py
+++ simulation/src/tests.py
@@ -2,8 +2,9 @@
 __author__ = "Simon Lassourreuille"
 
 # --------------------------------- imports ---------------------------------- #
-from main import Simulation
+from main import Simulation, make_symmetric
 import networkx as nx
+import matplotlib.pyplot as plt
 
 # --------------------------------- globals ---------------------------------- #
 N = 10  # default hyperparameter for stochastic testing
@@ -12,5 +13,14 @@ N = 10  # default hyperparameter for stochastic testing
 def test_init():
     # generating a bunch of graphs with nx
     for i in range(N):
-        er = nx.erdos_renyi_graph(100, 0.15)
-        sim = Simulation(er)
+        er = nx.erdos_renyi_graph(20, 0.15)
+        digraph = make_symmetric(er)
+        sim = Simulation(digraph)
+
+
+# --------------------------- interactive testing ---------------------------- #
+if __name__ == '__main__':
+    erdos_graph = nx.erdos_renyi_graph(20, 0.15)
+    digraph = make_symmetric(erdos_graph)
+    sim = Simulation(digraph)
+    sim.start()
