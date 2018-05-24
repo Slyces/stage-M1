c398dee6d5368a4a55caea6c7c0eaec77b39909c
Slyces <slyces.coding@gmail.com>
2018-05-24 15:06:14 +0200

    header complété, pas encore fini

--- simulation-c/include/network.h
+++ simulation-c/include/network.h
@@ -6,25 +6,78 @@
 #include <igraph.h>
 #include <stdio.h>
 
-// variables
+/* ─────────────────────────────── protocols ──────────────────────────────── */
+#define MSG 'm'
+#define CONF 'c'
+typedef char protocol;
+typedef pthread_t node_id;
 
-// structs
+/* ────────────────────────── adaptation functions ────────────────────────── */
+enum adapt_types {CV, EC, DC};
+typedef enum adapt_types adapt_type;
+
+struct adapt_function {
+    adapt_type type;
+    /* array of protocols, size and contents depends on the type of function:
+     *   - conversion: [from, to]
+     *   - encapsulation: [from, to] 
+     *   - decapsulation: [from, to] */
+    protocol * protocols;
+};
+typedef struct adapt_function adapt_f;
+
+/* ---------------- adaptation function creation and display ---------------- */
+adapt_function create_adapt_function(protocol from, protocol to, adapt_type);
+
+void store_repr(adapt_f func, char * str);
+
+/* ──────────────────────────────── messages ──────────────────────────────── */
+
+/* ------------------------ physical layer messages ------------------------- */
 struct physical_layer {
-
+    node_id sender;
+    node_id receiver;
+    char protocol;
+    void * content;
 };
 typedef struct physical_layer physical_message;
 
-struct conf_message {
 
+/* ------------------------- configuration messages ------------------------- */
+struct conf_msg {
+    node_id sender;
+    node_id dest;
+    int cost;
+    int stack_size;
+    protocol * protocol_stack;
 };
-typedef struct conf_message conf_message;
+typedef struct conf_msg conf_message;
 
-struct message {
-
+/* ---------------------------- regular messages ---------------------------- */
+struct msg {
+    node_id src;
+    node_id dest;
+    int stack_size;
+    int max_stack;
+    void * payload;  // literally anything
 };
-typedef struct message message;
+typedef struct msg message;
 
-// functions
+/* ──────────────────────────── nodes (routers) ───────────────────────────── */
+struct node {
+    node_id id;
+    adapt_f * adapt_functions; // array of adaptation functions
+    // stats
+    int received;
+    int sent;
+};
+typedef struct node router;
+
+/* -------------------------------------------------------------------------- */
+int static stack_size;
+int static running = 0;
+
+/* -------------------------------------------------------------------------- */
 
 /* emulation of sending a message through the physical layer
  *   - puts a physical_layer message in the receiver's stack */
@@ -46,6 +99,18 @@ void receive_msg(message msg);
 
 /* routing of a regular message */
 void route_msg(message msg);
+
+/* constant loop to receive physical messages, in a producer / consumer fashion */
+void messages_wait(void);
+
+/* initializes the network with the following parameters :
+ *   - graph: the topology of the network
+ *   - stack_size: the maximum size of a stack */
+void create_network(void * graph, int stack_size);
+
+/* starts the network
+ *   - max_time: time in seconds after which the network is stopped */
+void start_network(float max_time);
 
 /* main */
 int main(int argc, const char *argv[]);
