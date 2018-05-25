b082b43ad17c1dcb221a6d30c0f77c59a8862373
Slyces <slyces.coding@gmail.com>
2018-05-25 16:16:41 +0200

    en cours de gestion des créations / envois de messages

--- simulation-c/include/network.h
+++ simulation-c/include/network.h
@@ -15,7 +15,6 @@
 #define MSG 'm'
 #define CONF 'c'
 typedef char protocol;
-typedef pthread_t node_id;
 
 /* ────────────────────────── adaptation functions ────────────────────────── */
 typedef enum adapt_types {CV, EC, DC} adapt_type;
@@ -38,34 +37,35 @@ void store_repr(adapt_f func, char * str);
 
 /* ------------------------ physical layer messages ------------------------- */
 typedef struct physical_layer {
-    node_id sender;
-    node_id receiver;
-    char protocol;
+    int sender;
+    int receiver;
+    char prot;
     void * content;
 } physical_message;
 
 
 /* ------------------------- configuration messages ------------------------- */
 typedef struct conf_msg {
-    node_id sender;
-    node_id dest;
+    int sender;
+    int dest;
     int cost;
     int stack_size;
     stackT * protocol_stack;
 } conf_message;
 
 /* ---------------------------- regular messages ---------------------------- */
-typedef struct msg {
-    node_id src;
-    node_id dest;
+typedef struct msg_struct {
+    int src;
+    int dest;
     int stack_size;
     int max_stack;
     void * payload;  // literally anything
 } message;
 
+physical_message * create_physical(int sender, int receiver, char type, void * content);
 /* ──────────────────────────── nodes (routers) ───────────────────────────── */
 typedef struct router {
-    node_id id;
+    int id;
     int n_adapt;
     adapt_f * adapt_functions; // array of adaptation functions
     stackT * messages_stack;
@@ -94,24 +94,27 @@ int get_id(network * net);
 
 /* emulation of sending a message through the physical layer
  *   - puts a physical_layer message in the receiver's stack */
-void send(pthread_t sender_id, pthread_t receiver_id, void * message);
+void send(int sender, int receiver, message * msg);
+
+/* same as send, but for conf_message */
+void send_conf(int sender, int receiver, conf_message * msg);
 
 /* emulates the reception of a message through the physical layer
  *   - reads the physical message, converts the inner message to the
  *     correct type
  *   - calls the associated handler (receive_conf, receive_msg, route_msg) */
-void receive(physical_message phys);
+void receive(network * net, physical_message * phys);
 
 /* reception of a config message. 
  *   - adds rows to the routign table if necessary
  *   - send a message to each neighbor for each new route created */
-void receive_conf(conf_message msg);
+void receive_conf(network * net, int sender, conf_message * msg);
 
 /* reception of a regular message, destined to this node */ 
-void receive_msg(message msg);
+void receive_msg(network * net, message * msg);
 
 /* routing of a regular message */
-void route_msg(message msg);
+void route_msg(network * net, message * msg);
 
 /* initialises the node (router), then calls messages_wait */
 void * start_node(void * ptr);
@@ -124,9 +127,23 @@ void messages_wait(network * net);
 
 /* initializes the network with the following parameters :
  *   - graph: the topology of the network
+ *   - nodes_array: an array of node structures
  *   - stack_size: the maximum size of a stack 
  *   - max_time: the network will stop running after max_time seconds if max_time > 0 */
-void create_network(void * graph, int stack_size, float max_time);
+network * create_network(void * graph, node ** nodes_array, int stack_size);
+
+/* runs the network during at most max_time seconds */
+void start_network(network * net, float max_time);
+
+/* initializes a node */
+node * create_node(int id, adapt_f * functions, int n_functions);
+
+/* generates a random network with:
+ *   - n: number of nodes
+ *   - nb_prot: number of protocols available
+ *   - p: chance for each possible function to be known by a node
+ *   - max_stack: maximum stack_size of a route */
+network * random_network(int n, float p, int nb_prot, int max_stack);
 
 /* main */
 int main(int argc, const char *argv[]);
--- simulation-c/src/network.c
+++ simulation-c/src/network.c
@@ -1,13 +1,9 @@
 #include "network.h"
 #include <unistd.h>
 
-struct arg_container {
-    network * net;
-    int n_adapt;
-    adapt_f * adapt_functions;
-};
-/* ───────────────────────────── nodes behavior ───────────────────────────── */
+#define N 5
 
+/* ───────────────────────────── nodes behavior ───────────────────────────── */
 int get_id(network * net) {
     pthread_t thread_id = pthread_self();
     for (int i = 0; i < net->n; i++) {
@@ -20,28 +16,11 @@ int get_id(network * net) {
 /* ----------------------------- start the node ----------------------------- */
 void * start_node(void * ptr) {
     /* ------------------------ retrieving arguments ------------------------ */
-    struct arg_container * cont = (struct arg_container *) ptr;
-    network * net = cont->net;
-
-
-
+    network * net = (network *) ptr;
 
     /* ---------------------- little printf for debug ----------------------- */
     int my_id = get_id(net);
     printf("Hello world, I'm the thread %d\n", my_id);
-
-    /* ---------------- creating the stack for communication ---------------- */
-    stackT * stack = malloc(sizeof(stack));
-    StackInit(stack, MAX_SIZE);
-
-    /* ---------------------- creating the node object ---------------------- */
-    node * me = malloc(sizeof(node));
-
-    me->n_adapt = cont->n_adapt;
-    me->adapt_functions = cont->adapt_functions;
-    me->messages_stack = stack;
-
-    net->nodes[my_id] = me;
 
     /* ------------------------ waiting for messages ------------------------ */
     messages_wait(net);
@@ -51,15 +30,80 @@ void * start_node(void * ptr) {
 /* --------------------------- wait for messages ---------------------------- */
 void messages_wait(network * net) {
     int my_id = get_id(net);
+    node * me = net->nodes[my_id];
     while (net->running) {
-        sleep(1);
+        sleep(0.25);
+        while (!StackIsEmpty(me->messages_stack)) {
+            receive(net, (physical_message *) StackPop(me->messages_stack));
+        }
+        /*for (int i )*/
         printf("Woop ! (%d) here !\n", my_id);    
     }
 }
 
+/* ──────────────────────────── receive messages ──────────────────────────── */
+void receive(network * net, physical_message * phys) {
+    if (phys->prot == CONF) {
+        receive_conf(net, phys->sender, (conf_message *) phys->content);
+    } else if (phys->prot == MSG) {
+        message * msg = (message *) phys->content;
+        if (msg->dest == get_id(net)) {
+            receive_msg(net, msg);
+        } else {
+            route_msg(net, msg);
+        }
+    }
+    free(phys);
+}
+
+void receive_msg(network * net, message * msg) {
+    // -----------------------------------------
+    free(msg);
+}
+
+void receive_conf(network * net, int sender, conf_message * msg) {
+    // -----------------------------------------------------------
+    free(msg);
+}
+
+/* ───────────────────────────── route messages ───────────────────────────── */
+void route_msg(network * net, message * msg) {
+    // ---------------------------------------
+}
+
+/* ───────────────────────────── send messages ────────────────────────────── */
+/*void send()*/
+
+/* ─────────────────────────── physical messages ──────────────────────────── */
+/* ----------------------- create a physical message ------------------------ */
+physical_message * create_physical(int sender, int receiver, char prot, void * content) {
+    physical_message * p_msg = malloc(sizeof(physical_message));
+    p_msg->sender = sender;
+    p_msg->receiver = receiver;
+    p_msg->prot = prot;
+    p_msg->content = content;
+    return p_msg;
+}
+
+/* ─────────────────────────── creation of nodes ──────────────────────────── */
+node * create_node(int id, adapt_f * functions, int n_functions) {
+    node * new_node = malloc(sizeof(node));
+    new_node->id = id;
+
+    /* ---------------------- adaptation functions ---------------------- */
+    new_node->n_adapt = n_functions;
+    new_node->adapt_functions = functions;
+
+    /* ------------------------------- stack -------------------------------- */
+    stackT * new_stack = malloc(sizeof(new_stack));
+    StackInit(new_stack, MAX_SIZE);
+    new_node->messages_stack = new_stack;
+    return new_node;
+}
+
 /* ─────────────────── creation and start of the network ──────────────────── */
-void create_network(void * graph, int stack_size, float max_time) {
-    int n = 10;
+network * create_network(void * graph, node ** nodes, int stack_size) {
+    int n = N;
     pthread_t * pthreads_array = malloc(sizeof(pthread_t) * n);
     
     /* ------------------- creation of the network struct ------------------- */
@@ -67,39 +111,66 @@ void create_network(void * graph, int stack_size, float max_time) {
     net->n = n;
     net->graph = graph;
     net->threads = pthreads_array;
-    net->running = 1;
-    net->nodes = malloc(sizeof(node *) * n);
+    net->nodes = nodes;
 
+    return net;
+}
+
+void start_network(network * net, float max_time) {
+    /* --------------------- set the network to running --------------------- */
+    net->running = 1;
+    int n = net->n;
     /* ---------------------- creation of the threads ----------------------- */
     for (int i = 0; i < n; i++) {
-        struct arg_container cont = {net, 0, (adapt_f *) NULL};
-        struct arg_container * p_cont = &cont;
-        pthread_create(&pthreads_array[i], NULL, start_node, (void *) p_cont);
-        printf("Created thread %d with id %lu\n", i, pthreads_array[i]);
+        /* ------------ passing arguments to the thread creation ------------ */
+        pthread_create(&net->threads[i], NULL, start_node, (void *) net);
+        printf("Created thread %d with id %lu\n", i, net->threads[i]);
     }
 
+    /* --------------------- end of network conditions ---------------------- */
     sleep(max_time);
 
+    /* ------------------------ stopping the threads ------------------------ */
     net->running = 0;
-
-    /* -------------------- end condition of the network -------------------- */
     for (int i = 0; i < net->n; i++) {
         pthread_join(net->threads[i], NULL);
     } 
+
+    /* --------------------------- memory freeing --------------------------- */
     printf("# ---------- clearing memory --------- #\n");
     for (int i = 0; i < net->n; i++) {
         StackDestroy(net->nodes[i]->messages_stack);
         free(net->nodes[i]->messages_stack);
         free(net->nodes[i]);
     }
-    free(pthreads_array);
+    free(net->threads);
     free(net->nodes);
     free(net);
     printf("# ==================================== #\n");
 }
 
+network * random_network(int n, float p, int nb_prot, int max_stack){
+    /* ------------------------- creating the graph ------------------------- */
+    void * graph = NULL;
+
+    /* ----------------- creating the adaptation functions ------------------ */
+    
+    /* ------------------------- creating the nodes ------------------------- */
+    node ** nodes = malloc(sizeof(node *) * n);
+    for (int i = 0; i < n; i++) {
+        /* ----------------- choosing adaptation functions ------------------ */
+
+        // --
+        nodes[i] = create_node(i, NULL, 0);
+    }
+    return create_network(graph, nodes, max_stack);    
+}
+
 /* ────────────────────────────────── main ────────────────────────────────── */
 int main(int argc, const char * argv[]) {
     printf("# ==================================== #\n");
-    create_network(NULL, 10, 4);    
+    int n = N;
+    /* ----------------- creation and start of the network ------------------ */
+    network * net = random_network(n, 0, 4, 1);    
+    start_network(net, 2);
 }
